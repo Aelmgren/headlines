7e4b377ac1def018aefbca419f6a85d98fbad615
Anders Elmgren <anders.e.us@gmail.com>
2016-05-18 17:10:01 -0500

    dynamic routing + sailor moon

--- headlines.py
+++ headlines.py
@@ -1,1 +1,32 @@
+
+# -*- coding: utf-8 -*-
+
+import feedparser
+
 from flask import Flask
+
+app = Flask(__name__)
+
+RSS_FEEDS = { 'cc': 'http://blog.cheapcaribbean.com/rss',
+              'applevac': 'http://www.applevacationsblog.com/feed/',
+              'travelpulse': 'http://www.travelpulse.com/rss/news.rss?category=5123',
+              'skift': 'https://skift.com/feed/'}
+
+CC_FEED = "http://blog.cheapcaribbean.com/rss"
+
+@app.route("/")
+@app.route("/<publication>")
+
+def get_news(publication="cc"):
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    # for article in feed['entries']:
+
+    first_article = feed['entries'][0]
+    title = first_article.get("title")
+    published = first_article.get("published")
+    summary = first_article.get("summary").replace(u"\u2018","'").replace(u"\u2019", "'")
+
+    return """<html><body style="background-image:url(http://cdn1.sciencefiction.com/wp-content/uploads/2014/03/sailor_moon_by_anouet-d5e58cu.png);"><h1> CC Blog Feed</h1><div style='display:block;'><a href="/cc">CheapCaribbean.com Blog</a><a href="/travelpulse">Travel Pulse Blog</a><a href="/applevac">Apple Vacations Blog</a><a href="/skift">Skift Blog</a></div><b>{0}</b><br/><i>{1}</i><br/><p>{2}</p><br/></body></html>""".format(title, published, summary)
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
